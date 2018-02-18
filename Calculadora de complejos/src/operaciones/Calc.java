153ca7b9ec3c21af3657666ced09b490e1145b96
David BM <davbetm@gmail.com>
2018-02-17 20:43:46 -0600

    Display in polar form and exponential form: Complex numbers

--- Calculadora de complejos/src/operaciones/Calc.java
+++ Calculadora de complejos/src/operaciones/Calc.java
@@ -1,19 +1,26 @@
 package operaciones;
 /**
- *
  * @author david
+ * Esta clase contiene las operaciones básicas, cada una recibe dos números
+ * complejos con los que se va a operar, y por defecto retorna la instancia de un número
+ * complejo que representa el resultado en su forma algebraica.
  */
+
 public class Calc {
-    
+    // ============== SUMA
     public static Complejo sumar(Complejo A, Complejo B) {
+        // Atributos a calcular para contruir el número complejo resultado
         float resReal, resImaginario;
         
+        // Se hacen los cálculos, en este caso la suma => (x1+x2) + i(y1+y2)
         resReal = A.getParteReal() + B.getParteReal();
         resImaginario = A.getParteImaginaria() + B.getParteImaginaria();
         
+        // Se retorna un objeto Complejo.
         return new Complejo(resReal, resImaginario);
     }
     
+    // ============== RESTA => (x1-x2) + i(y1-y2)
     public static Complejo restar(Complejo A, Complejo B) {
         float resReal, resImaginario;
         
@@ -23,19 +30,21 @@ public class Calc {
         return new Complejo(resReal, resImaginario);
     }
     
+    // ============== MULTIPLICACIÓN => (x1x2 - y1y2) + i(x1y2 - x2y1)
     public static Complejo multiplicar(Complejo A, Complejo B) {
         float resReal, resImaginario;
         
-        // (x1x2 - y1y2)
         resReal = A.getParteReal() * B.getParteReal() - 
                 (A.getParteImaginaria() * B.getParteImaginaria());
-        // i(x1y2 + x2y1)
+        
         resImaginario = (A.getParteReal() * B.getParteImaginaria()) + 
                 (B.getParteReal() * A.getParteImaginaria());
         
         return new Complejo(resReal, resImaginario);
     }
     
+    // ============== DIVISIÓN => [ (x1x2 + y1y2) / (x2^2 + y2^2) ] +  
+        // i [ (x2y1 - x1y2) / (x2^2 + y2^2) ]
     public static Complejo dividir(Complejo A, Complejo B) {
         float resReal, resImaginario, denominador;
         
@@ -54,7 +63,7 @@ public class Calc {
         return new Complejo(resReal, resImaginario);
     }
     
-    // Genera una string con la representación de un número complejo
+    // Genera una string con la representación de un número complejo => x + iy
     public static String mostrarNumero(Complejo complejo) {
         float real = complejo.getParteReal();
         float imaginaria = complejo.getParteImaginaria();
@@ -67,6 +76,24 @@ public class Calc {
         resultado += (imaginaria != 1) ? Float.toString(imaginaria) : "";
         
         return resultado;
+    }
+    
+    // Genera una string con la forma trigonométrica de un complejo
+    public static String mostrarFormaTrigonometrica(Complejo complejo) {
+        String numero, r, angulo;
+        r = String.format("%.2f", complejo.getModulo());
+        angulo = String.format("%.2f", complejo.getAngulo());
+        numero = r + "[cos(" + angulo + "°) + isen(" + angulo + "°)]";
+        return numero;
+    }
+    
+    // Genera una string con la forma exponencial o de Euler de un complejo
+    public static String mostrarFormaExponencial(Complejo complejo) {
+        String numero, r, angulo;
+        r = String.format("%.2f", complejo.getModulo());
+        angulo = String.format("%.2f", complejo.getAngulo());
+        numero = r + "e^i" + angulo;  
+        return numero;
     }
     
 }
--- Calculadora de complejos/src/operaciones/TestCalc.java
+++ Calculadora de complejos/src/operaciones/TestCalc.java
@@ -1,13 +1,14 @@
 package operaciones;
-
 /**
- *
  * @author david
+ * Pruebas de las clases Calc y Complejo
  */
 public class TestCalc {
     public static void main(String []args) {
+        // Se instancian dos números complejos
         Complejo c1 = new Complejo(-3, 5);
         Complejo c2 = new Complejo(13, -7);
+        Complejo c3 = new Complejo(1, 1);
 
         Complejo res = Calc.sumar(c1, c2);   
         System.out.println("Suma c1+c2: " + Calc.mostrarNumero(res));
@@ -21,5 +22,13 @@ public class TestCalc {
         res = Calc.dividir(c1, c2);
         System.out.println("División c1/c2: " + Calc.mostrarNumero(res));
         
+        res = Complejo.conjugar(c2);
+        System.out.println("El conjugado de 13 - i7 es: " + Calc.mostrarNumero(res));
+        
+        System.out.println("Forma trigonométrica de 1 + i: " + 
+                Calc.mostrarFormaTrigonometrica(c3));
+        
+        System.out.println("Forma exponencial de 1 + i: " + 
+                Calc.mostrarFormaExponencial(c3));
     }
 }
