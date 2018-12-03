0b3ecd30fc9d21ec837f9a37ae7a63c055fffa57
DavidBM <davbetm@gmail.com>
2018-12-03 08:13:10 -0600

    Comentarios función serie de Fourier

--- MatLab/fourier.m
+++ MatLab/fourier.m
@@ -1,9 +1,10 @@
-function y = fourier( t )
+function y = fourier( t ) % función para calcular la serie truncada de Fourier
     y = 0.0;
     for n=1:10
         y = y + (((2*((-1)^n))-2) / (n*pi)) * sin(2*n*pi*t);
     end
 end
 % en consola poner:
-% t = -0.5:0.01:0.5, y = fourier(t), plot(t, y), xlabel('t'), ylabel('f(t)'), title('n = 300');
+% t = -0.5:0.01:0.5, y = fourier(t), plot(t, y), xlabel('t'), ylabel('f(t)'), title('n = ###');
+% t es el intervalo en el cual queremos graficar
 
